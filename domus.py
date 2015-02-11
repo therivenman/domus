255f20a47517aee9726d28b2bd98150c92fd6a0b
Matthew Wettlaufer <therivenman@gmail.com>
2015-02-11 13:15:10 -0800

    Updated TimeUtils class

--- domus.py
+++ domus.py
@@ -11,42 +11,48 @@ __version__ = "0.0.1"
 cityName = 'Los Angeles'
 timeZone = 'US/Pacific'
 
-class LocalTime(object):
+class TimeUtils(object):
     def __init__(self):
         # Setup Astral
-        self.astralInstance = Astral()
-        self.astralInstance.solar_depression = "civil"
-        self.astralCity = self.astralInstance[cityName]
+        self.__astralInstance = Astral()
+        self.__astralInstance.solar_depression = "civil"
+        self.__astralCity = self.__astralInstance[cityName]
 
-        self.days = {"Monday" : 0, "Tuesday" : 1, "Wednesday" : 2, "Thursday" : 3, "Friday" : 4, "Saturday" : 5, "Sunday" : 6,
+        self.__days = {"Monday" : 0, "Tuesday" : 1, "Wednesday" : 2, "Thursday" : 3, "Friday" : 4, "Saturday" : 5, "Sunday" : 6,
                      "Mon" : 0, "Tues" : 1, "Wed" : 2, "Thurs" : 3, "Fri" : 4, "Sat" : 5, "Sun" : 6 }
 
-    def now(self):
-        return self.currentTime
+        # Update time once to fill __currentTime variable
+        self.updateTime()
 
-    def todayIs(self, day):
+    def now(self):
+        return self.__currentTime
+
+    def sunTimes(self):
+        return self.__sunTime
+
+    def isToday(self, day):
         if type(day) is str:
             try:
-                return (self.days[day.capitalize()] == self.currentTime.weekday())
+                return (self.__days[day.capitalize()] == self.__currentTime.weekday())
             except KeyError:
                 return False;
         else:
-            return (day == self.currentTime.weekday())
+            return (day == self.__currentTime.weekday())
 
     def todayAt(self, hr, min = 0, sec = 0):
-        return self.currentTime.replace(hour=hr, minute=min, second=sec)
+        return self.__currentTime.replace(hour=hr, minute=min, second=sec)
 
     def minutesUntil(self, hr, min = 0, sec = 0):
-        timeDifference = self.todayAt(hr, min, sec) - a.now()
+        timeDifference = self.todayAt(hr, min, sec) - self.__currentTime
         return int(timeDifference.total_seconds() / 60)
 
     def updateTime(self):
-        self.currentTime = datetime.datetime.now(pytz.timezone(timeZone))
+        self.__currentTime = datetime.datetime.now(pytz.timezone(timeZone))
         if __verbose__:
-            print "Current Time: %s" % self.currentTime
+            print "Current Time: %s" % self.__currentTime
 
         # Then get todays sunset/sunrise times
-        self.sunTime = self.astralCity.sun(self.currentTime)
+        self.__sunTime = self.__astralCity.sun(self.__currentTime)
 
 def signalHandler(signal, frame):
     print "Exiting..."
