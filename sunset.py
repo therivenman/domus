5a65923419956df3f313c0cfeefa5ef9720c8058
Matthew Wettlaufer <therivenman@gmail.com>
2021-04-06 19:42:14 -0700

    Update sunset script

--- sunset.py
+++ sunset.py
@@ -2,6 +2,7 @@
 import domus
 import time
 import signal
+import sys
 
 def signalHandler(signal, frame):
     print "Exiting..."
@@ -11,9 +12,13 @@ def main():
     timeUtils = domus.TimeUtils()
     sleepTime = timeUtils.timeUntil(timeUtils.dusk())
 
+    if (sleepTime < 0):
+        print "Sunset has already passed for today."
+        sys.exit(1)
+
     signal.signal(signal.SIGINT, signalHandler)
 
-    print "Sunset at %s. Sleeping %d seconds." % (timeUtils.sunset(), sleepTime)
+    print "Dusk at %s. Sleeping %d seconds." % (timeUtils.dusk(), sleepTime)
     time.sleep(sleepTime)
 
 if __name__ == "__main__":
